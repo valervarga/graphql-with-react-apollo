edc2119a19e354836c271fc6fd79a36c26573505
Valer Varga <v.varga@vegaitsourcing.rs>
2019-08-13 11:26:52 +0200

    set proxy + set heroku deploy

---
+++ public/precache-manifest.93798689cd4f2477d10aa842776a9e57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d1aebd50c235b5a90c155c98e57ba36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74933c21a8dfeda7a85e",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "b9053a466f8428d4ab2c",
+    "url": "/static/js/2.ef68ba52.chunk.js"
+  },
+  {
+    "revision": "74933c21a8dfeda7a85e",
+    "url": "/static/js/main.cbfc198f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  }
+]);
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,5 +12,8 @@ app.use('/graphql', graphqlHTTP({
 	schema,
 	graphiql: true
 }));
+
+app.use(express.static('public'));
+app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'public', 'index.html')));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
