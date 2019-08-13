afe47e51445283f04d55cbfb56da235b58d1d077
Valer Varga <v.varga@vegaitsourcing.rs>
2019-08-13 09:08:39 +0200

    install cors + concurrently + apollo

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use('/graphql', graphqlHTTP({
 	schema,
 	graphiql: true
